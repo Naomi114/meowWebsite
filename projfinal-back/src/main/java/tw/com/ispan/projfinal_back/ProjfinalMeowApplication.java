40987325a866fbf0bea5a0837e7a3debdc379980
Naomi <nchou04@gmail.com>
2025-01-05 14:50:35 +0800

    更新設定檔

--- projfinal-back/src/main/java/tw/com/ispan/projfinal_back/ProjfinalMeowApplication.java
+++ projfinal-back/src/main/java/tw/com/ispan/projfinal_back/ProjfinalMeowApplication.java
@@ -1,4 +1,4 @@
-package tw.com.ispan.projfinal_meow;
+package tw.com.ispan.projfinal_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- projfinal-back/src/main/java/tw/com/ispan/projfinal_back/ServletInitializer.java
+++ projfinal-back/src/main/java/tw/com/ispan/projfinal_back/ServletInitializer.java
@@ -1,4 +1,4 @@
-package tw.com.ispan.projfinal_meow;
+package tw.com.ispan.projfinal_back;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
