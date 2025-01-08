0d302609488f430370ef60240f97980963117ab7
Naomi <nchou04@gmail.com>
2025-01-08 18:42:21 +0800

    更新import

--- projfinal-back/src/main/java/tw/com/ispan/projfinal_back/domain/shop/TagBean.java
+++ projfinal-back/src/main/java/tw/com/ispan/projfinal_back/domain/shop/TagBean.java
@@ -2,8 +2,6 @@ package tw.com.ispan.projfinal_back.domain.shop;
 
 import java.util.List;
 
-import org.hibernate.annotations.ManyToAny;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,7 +9,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
