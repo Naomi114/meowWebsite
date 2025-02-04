e2f398d2b1b160a2c1af48450ad4fbcd2d780902
Joylin <joylin8811@gmail.com>
2025-02-04 12:50:34 +0800

    新增註解

--- projfinal-back/src/main/java/tw/com/ispan/SpringBootConfig.java
+++ projfinal-back/src/main/java/tw/com/ispan/SpringBootConfig.java
@@ -9,8 +9,8 @@ public class SpringBootConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         /* 將前後端頁面註冊後，CORS可以跨過SOP限制 */
-        // registry.addMapping("/ajax/pages/products/**")
-        // .allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/lostcases/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
 
         // registry.addMapping("/ajax/secure/login");
     }
--- projfinal-back/src/main/java/tw/com/ispan/controller/BannerController.java
+++ projfinal-back/src/main/java/tw/com/ispan/controller/BannerController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import tw.com.ispan.domain.pet.Banner.BannerType;
 import tw.com.ispan.service.banner.BannerService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/lostBanners")
 public class BannerController {
 
