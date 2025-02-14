96da7ce3dc0a145ed10786d118f63369d49da0db
Naomi114 <nchou04@gmail.com>
2025-02-14 18:17:37 +0800

    新增 建立db sql指令

---
+++ projfinal-back/src/main/java/tw/com/ispan/model/PaymentRequest.java
@@ -0,0 +1,38 @@
+// package tw.com.ispan.model;
+
+// public class PaymentRequest {
+
+// private double amount; // 支付金额
+// private String description; // 交易描述
+// private String itemName; // 商品名称
+
+// // Getters 和 Setters
+// public double getAmount() {
+// return amount;
+// }
+
+// public void setAmount(double amount) {
+// this.amount = amount;
+// }
+
+// public String getDescription() {
+// return description;
+// }
+
+// public void setDescription(String description) {
+// this.description = description;
+// }
+
+// public String getItemName() {
+// return itemName;
+// }
+
+// public void setItemName(String itemName) {
+// this.itemName = itemName;
+// }
+
+// public String getOrderId() {
+// // TODO Auto-generated method stub
+// throw new UnsupportedOperationException("Unimplemented method 'getOrderId'");
+// }
+// }
